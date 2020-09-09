615cba6c9cc878be9d7d59fc3f68e7775ee6e5a0
Harrison Klein <harrisonklein123@gmail.com>
2020-09-09 15:18:39 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        String token = "NzUzMzE2MDM3MzE4OTM0NjI5.X1kaOw.paAaQZcUggye32PliJVniCjGET4";
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.addEventListeners(new Main());
+        builder.build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        //first line ignores input from bots, including itself
+        if(event.getAuthor().isBot()) {
+            return;
+        }
+        System.out.println("We received a message from " +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay());
+        if(event.getMessage().getContentRaw().equals("!ping")) {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
+    }
+}
