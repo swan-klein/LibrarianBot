ab4d9b6f4adf1da06eccc69a0624bf01110a5e8a
harry-swan <hklein2@masonlive.gmu.edu>
2020-11-09 23:12:34 -0500

    Michael's merged code

--- src/main/java/Librarian/Book.java
+++ src/main/java/Librarian/Book.java
@@ -1,7 +1,9 @@
 package Librarian;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.io.*;
+
 public class Book {
 
 private String title, description;
@@ -9,7 +11,7 @@ private List<String> authors = new ArrayList<>();
 private double rating;
 
 public Book() {
-	
+
 }
 
 public Book(String title, ArrayList<String> authors, String description, double rating) {
@@ -53,4 +55,73 @@ public Book(String title, ArrayList<String> authors, String description, double 
 	    this.description = description;
 	}
 
+	public static String quoteSearch(String quote) throws Exception {
+		org.jsoup.nodes.Document doc = org.jsoup.Jsoup.connect("https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q=" + quote + "&commit=Search").get();
+		org.jsoup.select.Elements els = doc.getElementsByClass("authorOrTitle");
+		StringBuilder elList = new StringBuilder();
+		int index = 0;
+		for (org.jsoup.nodes.Element el : els) {
+			if (!elList.toString().contains(el.text())) {
+				elList.append(el.text());
+				if (index++ % 2 == 0) {
+					elList.append(" ");
+				} else {
+					elList.append("\n");
+				}
+			}
+		}
+		return elList.toString();
+	}
+
+
+	public static String wordFrequency(String book, String[] words, String path) throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader(new File(path.substring(0, path.length()-1) + "GUTINDEX.ALL")));
+		String booknum;
+		while ((booknum = br.readLine()) != null) {
+			if (booknum.contains(book) && booknum.length() > 73) {
+				System.out.println(booknum);
+				booknum = booknum.substring(73).replaceAll("[^0-9]", "");
+				break;
+			}
+		}
+		if (booknum == null) return "No book with the title " + book + " was found";
+		StringBuilder url = new StringBuilder("https://mirrors.xmission.com/gutenberg/");
+		if (booknum.length() == 1) url.append("0/");
+		for (int i = 0; i < booknum.length()-1; i++) {
+			url.append(booknum.charAt(i));
+			url.append("/");
+		}
+		booknum += "/" + booknum + ".txt";
+		url.append(booknum);
+		int[] freqs = new int[words.length];
+		for (int i = 0; i < words.length; i++) {
+			freqs[i] = count_freq(org.jsoup.Jsoup.connect(url.toString()).get().getElementsByTag("body").get(0).text(), words[i]);
+		}
+		System.out.println(words[0] + freqs[0]);
+		StringBuilder ret = new StringBuilder();
+		for (int i = 0; i < words.length; i++) {
+			ret.append(words[i]);
+			ret.append(": ");
+			ret.append(freqs[i]);
+			if (i != words.length - 1) ret.append("\n");
+		}
+		return ret.toString();
+	}
+
+	// Function to count the frequency of
+	// a given word in the given string
+	static int count_freq(String str, String word) {
+
+		int freq = 0;
+		// Splitting to find the word
+		String[] arr = str.split("[\\W]");
+
+		// Loop to iterate over the words
+		for(String w : arr)
+		{
+			if (w.equalsIgnoreCase(word)) freq++;
+		}
+
+		return freq;
+	}
 }
--- src/main/java/Librarian/Main.java
+++ src/main/java/Librarian/Main.java
@@ -2,8 +2,13 @@ package Librarian;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +16,7 @@ import java.util.List;
 import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
+	static Path workingDirectory;
 	
 	public enum Comms 
 	{
@@ -37,14 +43,17 @@ public class Main extends ListenerAdapter {
         JDABuilder builder = JDABuilder.createDefault(token);
         builder.addEventListeners(new Main());
         builder.build();
+		workingDirectory=Paths.get(".").toAbsolutePath();
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+		//first line ignores input from bots, including itself
+		if(event.getAuthor().isBot()) {
+			return;
+		}
         String[] compliment = {"Awesome","Legendary","Crazy","Insane","Godly","Professional","Prominent","Prodigy","Intellectual"};
-        
-   
- 
+
     	String command = event.getMessage().getContentRaw();
     	int index = command.indexOf(' ');
     	String help = " ";
@@ -62,7 +71,11 @@ public class Main extends ListenerAdapter {
     		
     		command = " ";
     	  }
-    	
+
+		//processes input to produce desired output
+		System.out.println("We received a message from " +
+				event.getAuthor().getName() + ": " +
+				event.getMessage().getContentDisplay());
     	try {
     		
     	    ArrayList<Book> books;  
@@ -96,9 +109,35 @@ public class Main extends ListenerAdapter {
    		     bd.addField("```!author [book name]```", "Prints the author(s) of the book.\n", false);
    		     bd.addField("```!description [book name]```", "Prints a short description of a book.", true);
    		     bd.addField("```!rating [book name]```", "Prints the review rating of a book.", false);
-   		     bd.setColor(new Color(8311585));
+   		     bd.setColor(new Color(0x7ED321));
    			 event.getChannel().sendMessage(bd.build()).queue();
     		}
+    		else if (event.getMessage().getContentRaw().substring(0, event.getMessage().getContentRaw().indexOf(' ')).equals("!booksBy")) {
+				String author = event.getMessage().getContentRaw();
+				author = author.substring(author.indexOf(' ') + 1);
+				try {
+					event.getChannel().sendMessage(Author.booksBy(author, workingDirectory.toString())).queue();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (event.getMessage().getContentRaw().substring(0, event.getMessage().getContentRaw().indexOf(' ')).equals("!quoteSearch")) {
+				String quote = event.getMessage().getContentRaw();
+				quote = quote.substring(quote.indexOf(' ') + 1);
+				try {
+					event.getChannel().sendMessage(Book.quoteSearch(quote)).queue();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (event.getMessage().getContentRaw().substring(0, event.getMessage().getContentRaw().indexOf(' ')).equals("!wordFrequency")) {
+				String book = event.getMessage().getContentRaw();
+				book = book.substring(book.indexOf(' ') + 1);
+				String[] words = book.substring(book.indexOf(';') + 1).trim().split("[\\W]");
+				try {
+					event.getChannel().sendMessage(Book.wordFrequency(book.substring(0,book.indexOf(";")), words, workingDirectory.toString())).queue();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
     	  
 		} catch (Exception e) {
 			e.printStackTrace();
